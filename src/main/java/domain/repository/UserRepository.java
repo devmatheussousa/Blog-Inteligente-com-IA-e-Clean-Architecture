7fd28ebaf341d0dc2a43e4c22c421834e949b9c2
DevMatheusSousa <dev.matheus.contato@gmail.com>
2026-03-03 20:36:54 -0300

    thanks

--- src/main/java/domain/repository/UserRepository.java
+++ src/main/java/domain/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package domain.repository;
 
-public class UserRepository {
+import domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //busca por email
+    User findByEmail(String email);
 }
