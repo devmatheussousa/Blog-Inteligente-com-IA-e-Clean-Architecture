7c4b4af2599f5256d71a5285696d302f9cb9119a
DevMatheusSousa <dev.matheus.contato@gmail.com>
2026-03-11 13:27:42 -0300

    feat: Create and UserRepository

--- src/main/java/domain/entity/User.java
+++ src/main/java/domain/entity/User.java
@@ -1,4 +1,57 @@
 package domain.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+@EqualsAndHashCode
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @NotBlank
+    @Email
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(length = 500)
+    private String bio;
+
+    @Column(name = "fotoUrl")
+    private String fotoUrl;
+
+    private Role role;
+
+    @CreationTimestamp
+    @Column(name = "createdAt", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updatedAt")
+    private LocalDateTime updatedAt;
+
+    public enum Role{
+        USER,
+        ADMIN
+    }
 }
